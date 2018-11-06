ec65c47e89a7cc8ed2963696dcc9917d379aab4c
OlegKass <0leg221106@gmail.com>
2018-11-06 14:17:17 +0200

    Def 1

--- km_83/Kasyanovskyi_Oleg/workshop1/source/Def/Lab 1.py
+++ km_83/Kasyanovskyi_Oleg/workshop1/source/Def/Lab 1.py
@@ -1,1 +1,2 @@
 from km_83.Kasyanovskyi_Oleg.workshop1.source.Laba_1.Laba_1 import
+print(sqr(3))
--- km_83/Kasyanovskyi_Oleg/workshop1/source/Def/Lab 2.py
+++ km_83/Kasyanovskyi_Oleg/workshop1/source/Def/Lab 2.py
@@ -1,1 +1,2 @@
 from km_83.Kasyanovskyi_Oleg.workshop1.source.Laba_1.Laba_2 import
+print(Sum2(3,4))
